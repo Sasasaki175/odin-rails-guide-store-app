e767af5c33f48170e66a4086bb7c167a2c266208
Sasasaki175 <207643966+Sasasaki175@users.noreply.github.com>
2026-05-26 14:46:04 +0900

    fix: run pending Active Storage migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_05_14_012957) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_25_235505) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "products", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
+    t.string "name"
     t.datetime "updated_at", null: false
   end
 end
